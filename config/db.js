d562b42c9e4dca207843c008edeb992a147209c1
shubh <shubh95rai@gmail.com>
2025-09-03 01:15:43 +0530

    final

--- config/db.js
+++ config/db.js
@@ -1,10 +1,5 @@
 import mysql from "mysql2/promise";
 
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+const db = mysql.createPool(process.env.DATABASE_URL);
 
 export default db;
