adcf39aeb59c06f4b20aaafcb67116bbdf2e9703
shubh <shubh95rai@gmail.com>
2025-09-03 02:03:23 +0530

    final

--- config/db.js
+++ config/db.js
@@ -7,12 +7,7 @@
 export async function getDB() {
   const mysql = await import("mysql2/promise");
 
-  const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB,
-  });
+  const pool = mysql.createPool(process.env.DATABASE_URL);
 
   return pool;
 }
